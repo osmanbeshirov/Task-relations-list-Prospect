6d6af5d8c47384c28291447cd864d4e2655b39eb
osmanbeshirov <osman.beshirov@gmail.com>
2022-08-30 20:36:15 +0400

    new form is added

--- src/App.js
+++ src/App.js
@@ -5,21 +5,32 @@ import Header from './containers/Header/Header';
 import NewRelation from './components/NewRelation/NewRelation';
 import Contacts from './containers/Contacts/Contacts';
 
+import { Routes, Route } from 'react-router-dom'
+import NewContacts from './containers/NewContacts/NewContacts';
+
 function App() {
   return (
     <div className='app' >
       <div className='container'>
-
         <Row>
           <Col span={24}>
             <Header />
           </Col>
         </Row>
+
         <Row>
           <Col span={24}>
-            <Contacts />
+            <Routes>
+              <Route path='/' element={<Contacts />} />
+              <Route path='/contacts' element={<Contacts />} />
+              <Route path='/contacts/new' element={<NewContacts />} />
+            </Routes>
+
           </Col>
         </Row>
+
+
+
 
       </div>
     </div>
